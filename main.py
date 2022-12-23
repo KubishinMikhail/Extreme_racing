b2158d1a5a76dd8a1d13ea10d3e2060c1b308cd7
KubishinMikhail <kubishin.m@gmail.com>
2022-12-23 18:34:30 +0300

    Start

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+from main_car import MainCar
+
+
+pygame.init()
+sprite_of_car = pygame.sprite.GroupSingle()
+rotates = [pygame.image.load('pictures/main_car.png'),
+           pygame.image.load('pictures/main_car_rotate_1.png'),
+           pygame.image.load('pictures/main_car_rotate_2.png'),
+           pygame.image.load('pictures/main_car_rotate_3.png'),
+           pygame.image.load('pictures/main_car_rotate_4.png')]
+road = pygame.image.load('pictures/road.png')
+
+
+if __name__ == '__main__':
+    size = width, height = 800, 1000
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Extreme racing')
+
+    fps = 60
+    clock = pygame.time.Clock()
+
+    car = MainCar((350, 500), sprite_of_car)
+    running = True
+    pos = True
+    count_of_frames = 0
+
+    while running:
+        if count_of_frames:
+            count_of_frames += 1
+            sprite_of_car.update(pos, count_of_frames)
+            if count_of_frames == 41:
+                count_of_frames = 0
+        screen.blit(road, (0, 0))
+        sprite_of_car.draw(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.KEYUP and not count_of_frames:
+                pos = event.key
+                count_of_frames = 1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                car.run_left = True
+                car.run_right = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                car.run_left = False
+                car.run_right = True
+        sprite_of_car.draw(screen)
+        pygame.display.flip()
+
+        clock.tick(fps)
+    pygame.quit()
---
+++ main_car.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+rotates = [pygame.image.load('pictures/main_car.png'),
+           pygame.image.load('pictures/main_car_rotate_1.png'),
+           pygame.image.load('pictures/main_car_rotate_2.png'),
+           pygame.image.load('pictures/main_car_rotate_3.png'),
+           pygame.image.load('pictures/main_car_rotate_4.png')]
+main_car = rotates[0]
+road = pygame.image.load('pictures/road.png')
+
+
+class MainCar(pygame.sprite.Sprite):
+    def __init__(self, pos, sprite_of_car):
+        super().__init__(sprite_of_car)
+        self.image = main_car
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.run_left = False
+        self.run_right = False
+
+    def update(self, position, count_of_frames_2):
+        if self.run_left and self.rect.centerx > 201:
+            if count_of_frames_2 < 5:
+                self.image = rotates[1]
+            elif 5 <= count_of_frames_2 < 10:
+                self.image = rotates[2]
+            elif 35 <= count_of_frames_2 < 40:
+                self.image = rotates[1]
+            elif count_of_frames_2 == 40:
+                self.image = rotates[0]
+            self.rect = self.rect.move(-5, 0)
+        if self.run_right and self.rect.centerx < 600:
+            self.rect = self.rect.move(5, 0)
+
